29b4bbff95e4e425730b720c595eeac4b8fad118
Ursula Cubilla <ursulacubilla@gmail.com>
2023-04-21 14:33:47 +0200

    Ejercicio1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { InputsCheckbox } from './Inputs';
 
 function App() {
+
+  const [count, setCount] = useState(
+    new Array(InputsCheckbox.length).fill(false)
+  ); // [],{},"",numeros,booleano "
+  console.log(InputsCheckbox);
+
+
+  const [total, setTotal] = useState(0);
+
+
+  const handleInputChange = (e) => {
+    const updatedCheckedState = count.map((item, index) =>
+      index === e ? !item : item
+    );
+
+    setCount(updatedCheckedState);
+
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + InputsCheckbox[index].price;
+        }
+        return sum;
+      },
+      0
+    );
+    setTotal(totalPrice);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Hola.
-        </p>
-      </header>
+
+      <p>¿Qué quieres hacer?</p>
+      {InputsCheckbox.map(({ name, price, text }, index) => {
+        return (
+
+          <section key={name}>
+            <input
+              name={name}
+              type="checkbox"
+              checked={count[index]}
+              onChange={() => handleInputChange(index)}
+            />
+            <label>{text} {price}</label>
+          </section>
+        );
+      })}
+
+      <p>Precio: {total}</p>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Inputs.js
@@ -0,0 +1,17 @@
+export const InputsCheckbox = [
+{
+    name: "One",
+    price: 500,
+    text: "Una página web "
+},
+{
+    name: "Two",
+    price: 300,
+    text: "Una consultoria SEO "
+},
+{
+    name: "Three",
+    price: 200,
+    text: "Una campaña de Google Ads "
+}
+];
